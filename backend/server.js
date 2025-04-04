6368c3daf18960ea33e5af34b58da96acd103a8f
alim509 <tul56193@temple.edu>
2025-04-03 21:00:59 -0600

    Configured models and routes and connected to server.js

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const taskRoutes = require("./routes/taskRoutes");
+app.use("/tasks", taskRoutes);
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
