78680d2914be26a95139536c997b553c4b1ff013
alim509 <tul56193@temple.edu>
2025-04-03 21:49:02 -0600

    Removed deprecated options and moved .env outside of backend

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,8 @@ app.use(cors());
 const taskRoutes = require("./routes/taskRoutes");
 app.use("/tasks", taskRoutes);
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
